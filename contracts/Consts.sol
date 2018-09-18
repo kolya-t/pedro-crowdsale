1ea294eff7e9c8b5b9f53c4347e48ea00962f4eb
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-09-18 12:20:31 +0300

    change hard cap and min investment

--- contracts/Consts.sol
+++ contracts/Consts.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.4.23;
 
 
 contract Consts {
-    uint public constant MIN_INVESTMENT = 100000000000000000; // 0.1 ETH
-    uint public constant USDCENTS_HARD_CAP = 100000000; // 1'000'000.00 USD
+    uint public constant MIN_INVESTMENT = 10000000000000000; // 0.01 ETH
+    uint public constant USDCENTS_HARD_CAP = 3000000000; // 1'000'000.00 USD
     uint public constant TOKEN_DECIMALS = 10;
     uint8 public constant TOKEN_DECIMALS_UINT8 = 10;
     uint public constant TOKEN_DECIMAL_MULTIPLIER = 10 ** TOKEN_DECIMALS;
